f7d4dded8eabcab8201e6c88bdb28f13634ef687
Leonidig <leonid200955@gmail.com>
2024-10-15 14:16:24 +0300

    calculator ( switch case + if )

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+using namespace std;
+
 
 int main() {
-    int userNumber;
-    std::cout << "Enter number: ";
-    std::cin >> userNumber;
+    int x;
+    int y;
+    int option;
 
-    for (int i = 1; i <= 10; i++) {
-        std::cout << i << " * " << userNumber << " = " << i * userNumber << std::endl;
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter y: ";
+    cin >> y;
+
+    cout << "Enter operation\n1 - addition\n2 - subtracting\n3 - division\n4 - multiplying\n>";
+    cin >> option;
+
+    switch (option) {
+    case 1:
+        cout << x << " + " << y << " = " << x + y;
+        break;
+    case 2:
+        cout << x << " - " << y << " = " << x - y;
+        break;
+    case 3:
+        if (y != 0) {
+            cout << x << " : " << y << " = " << x / y;
+        }
+        else {
+            cout << "Error: Division by zero!";
+        }
+        break;
+    case 4:
+        cout << x << " * " << y << " = " << x * y;
+        break;
+    default:
+        cout << "Enter correct option!";
+        break;
     }
 
     return 0;
