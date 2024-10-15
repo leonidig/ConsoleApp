277ffda977b92e914911c8d43761a80d6e3e9528
Leonidig <leonid200955@gmail.com>
2024-10-15 15:08:40 +0300

    test

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -15,28 +15,33 @@ int main() {
 
     cout << "Enter operation\n1 - addition\n2 - subtracting\n3 - division\n4 - multiplying\n>";
     cin >> option;
+    
+    switch (option){
 
-    switch (option) {
-    case 1:
-        cout << x << " + " << y << " = " << x + y;
-        break;
-    case 2:
-        cout << x << " - " << y << " = " << x - y;
-        break;
-    case 3:
-        if (y != 0) {
-            cout << x << " : " << y << " = " << x / y;
-        }
-        else {
-            cout << "Error: Division by zero!";
-        }
-        break;
-    case 4:
-        cout << x << " * " << y << " = " << x * y;
-        break;
-    default:
-        cout << "Enter correct option!";
-        break;
+        case 1:
+            cout << x << " + " << y << " = " << x + y;
+            break;
+
+        case 2:
+            cout << x << " - " << y << " = " << x - y;
+            break;
+
+        case 3:
+            if (y != 0) {
+                cout << x << " : " << y << " = " << x / y;
+            }
+            else {
+                cout << "Error: Division by zero!";
+            }
+            break;
+
+       case 4:
+            cout << x << " * " << y << " = " << x * y;
+            break;
+
+       default:
+            cout << "Enter correct option!";
+            break;
     }
 
     return 0;
